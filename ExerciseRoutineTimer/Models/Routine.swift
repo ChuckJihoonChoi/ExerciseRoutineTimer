7d288381ea9526ba7664d13ce01335e7a616e98b
Nguyet Nga Nguyen (Irene) <143987122+irenenguyen8@users.noreply.github.com>
2025-05-08 18:32:59 +1000

    Update Routine.swift

--- ExerciseRoutineTimer/Models/Routine.swift
+++ ExerciseRoutineTimer/Models/Routine.swift
@@ -2,7 +2,7 @@
 //  Routine.swift
 //  FlexPlanDraft
 //
-//  Created by JH's macbook on 5/2/25.
+//  Created by Nguyet Nga Nguyen on 5/2/25.
 //
 import Foundation
 import SwiftData
@@ -11,25 +11,26 @@ import SwiftData
 class Routine {
     var id: UUID
     var name: String
-    var moduleId: [UUID]
     var restBetweenModules: Int
     var voiceEnabled: Bool
     var vibrationEnabled: Bool
     var createdAt: Date
     var lastExecutedAt: Date?
 
-    init(id: UUID = UUID(), name: String, moduleId: [UUID] = [],
-         restBetweenModules: Int = 0, voiceEnabled: Bool = false, vibrationEnabled: Bool = false,
+    // Relation between currernt routine and model
+    @Relationship(deleteRule: .cascade, inverse: \Module.routine)
+    var modules: [Module]
+    
+    init(id: UUID = UUID(), name: String, restBetweenModules: Int = 0, voiceEnabled: Bool = false, vibrationEnabled: Bool = false,
          createdAt: Date = .now, lastExecutedAt: Date? = nil) {
         self.id = id
         self.name = name
-        self.moduleId = moduleId
         self.restBetweenModules = restBetweenModules
         self.voiceEnabled = voiceEnabled
         self.vibrationEnabled = vibrationEnabled
         self.createdAt = createdAt
         self.lastExecutedAt = lastExecutedAt
+        self.modules = []
     }
 }
-
 
