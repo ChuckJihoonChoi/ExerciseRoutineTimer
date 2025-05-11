857f5356428e1b9233c6f91f41fe9ca30d6c2be1
ThuyMinh-Luu <thuyminh.luu@student.uts.edu.au>
2025-05-11 12:26:45 +1000

    Update StatisticView.swift

--- ExerciseRoutineTimer/Components/StatisticView.swift
+++ ExerciseRoutineTimer/Components/StatisticView.swift
@@ -1,1 +1,29 @@
+//
+//  StatisticView.swift
+//  FlexPlan1
+//
+//  Created by Thuy Minh Luu on 7/5/2025.
+//
+import SwiftUI
+import SwiftData
 
+struct StatisticView: View {
+    let title: String
+    let value: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.caption)
+                .foregroundColor(.secondary)
+            
+            Text(value)
+                .font(.title3)
+                .fontWeight(.semibold)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(10)
+    }
+}
